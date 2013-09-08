ca3c4b76792e92ff5921879504864476f34c3db3
alexander-turner <turneral1@grinnell.edu>
2013-09-08 16:22:26 -0500

    Update TestTwoA.java

--- src/edu/grinnell/csc207/turneral1/hw2/TestTwoA.java
+++ src/edu/grinnell/csc207/turneral1/hw2/TestTwoA.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestTwoA {
+	@Test
+	public void test_isMultiple() {
+		assertEquals("none")
+	} // test_isMultiple
+	
     @Test
     public void test_isOdd() {
 	assertEquals("zero", false, TwoA.isOdd(0));
@@ -14,7 +19,26 @@ public class TestTwoA {
     	    assertEquals("negative even", true, TwoA.isOdd(-2*i));
     	    assertEquals("negative odd", false, TwoA.isOdd(-1*i));
 	} // for
+	// INCLUDE TESTS FOR MAX INT
     } // test_isOdd
     
-
+    @Test
+    public void test_oddSumTo() {
+    	
+    } // test_oddSumTo
+   
+    @Test
+    public void test_isOddProd() {
+    	
+    } // test_isOddProd
+    
+    @Test
+    public void test_allDistinct() {
+    	assertEquals(")
+    } // test_allDistinct
+    
+    @Test
+    public void test_reverseInts() {
+    	
+    } // test_reverseInts
 }
